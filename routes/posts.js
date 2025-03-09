50ea39ea3fddccf9502f635c9e2a8fabdf76525c
ahmedhidar <ahmedheedar@gmail.com>
2025-03-09 23:44:05 +0200

    initial commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const postsController = require("./../controllers/posts");
 const auth = require("./../middlewares/auth");
+const restrictTo = require("./../middlewares/restrictTo");
+
 const router = express.Router();
 
+// Any authenticated user (user/admin) can create a post
 router.post("/", auth, postsController.createPost);
 
-router.get("/", postsController.getPosts);
+// Any authenticated user (user/admin) can get all posts
+router.get("/", auth, postsController.getPosts);
 
-router.get("/:id", postsController.getPost);
+// Any authenticated user (user/admin) can get a post by ID
+router.get("/:id", auth, postsController.getPost);
 
-router.patch("/:id", postsController.updatePost);
+// Only admin can update a post
+router.patch("/:id", auth, restrictTo("admin"), postsController.updatePost);
 
-router.delete("/:id", postsController.deletePost);
+// Only admin can delete a post
+router.delete("/:id", auth, restrictTo("admin"), postsController.deletePost);
 
 module.exports = router;
